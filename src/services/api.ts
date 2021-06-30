b0c6c87e5cd9e702ee24b178da20a39ef465822f
rooky6 <rooky6@users.noreply.github.com>
2021-06-30 17:45:24 -0300

    changing the api baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/romualdo-ah/rocketshoes_json_server',
+  baseURL: 'https://my-json-server.typicode.com/romualdo-ah/rocketseat-carrinho-de-compras',
 });
